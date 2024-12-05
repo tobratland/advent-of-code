871148f8771bfd908df8e3302cdaaecffd00e7cf
Torbjørn Bratland <torbjorn.bratland@bouvet.no>
2024-12-05 15:58:22 +0100

    feat: day 4 complete

---
+++ 2024/day_04/target/debug/deps/day_04-8110de4b5473a102.d
@@ -0,0 +1,5 @@
+/home/tobratland/workspace/github.com/tobratland/advent-of-code/2024/day_04/target/debug/deps/libday_04-8110de4b5473a102.rmeta: src/lib.rs
+
+/home/tobratland/workspace/github.com/tobratland/advent-of-code/2024/day_04/target/debug/deps/day_04-8110de4b5473a102.d: src/lib.rs
+
+src/lib.rs:
